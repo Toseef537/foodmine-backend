27ca18100a4290eaa414d8771ea0ebac5b9d6b1b
toseefdar <toseefdar537@gmail.com>
2024-11-11 05:08:36 -0800

    frontend url changed

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,12 +19,21 @@ app.use(express.json());
 
 app.use(cors({
     origin: 'https://foodmine-frontend-gamma.vercel.app',
-  }));
+    credentials: true,
+}));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://foodmine-frontend-gamma.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
 app.use("/api/foods", foodRouter)
-app.use("/api/users",userRouter)
-app.use("/api/carts",cartRouter)
-app.use("/api/orders",orderRouter)
+app.use("/api/users", userRouter)
+app.use("/api/carts", cartRouter)
+app.use("/api/orders", orderRouter)
 
 
 
