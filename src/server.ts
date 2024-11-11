f002f827e901bdb39efc92e3592a9da794eb88a2
toseefdar <toseefdar537@gmail.com>
2024-11-11 10:44:37 -0800

    add

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 import express from "express";
 import cors from "cors";
-import { sample_food, sample_tags, Sample_Users } from "./data";
-import foodRouter from "./routers/food.router"
-import userRouter from "./routers/user.router"
-import { dbConnect } from './configs/database.config';
-import orderRouter from './routers/order.router';
-import cartRouter from './routers/cart.router';
+import { sample_food, sample_tags, Sample_Users } from "../src/data";
+import foodRouter from "../src/routers/food.router"
+import userRouter from "../src/routers/user.router"
+import { dbConnect } from '../src/configs/database.config';
+import orderRouter from '../src/routers/order.router';
+import cartRouter from '../src/routers/cart.router';
 dotenv.config();
 dbConnect();
 const app = express();
@@ -27,5 +27,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
 })
 
-
+export default app;
 
