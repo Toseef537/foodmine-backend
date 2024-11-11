2e45f6972b7bbd408f01cd476cde3d1e09dd43c4
toseefdar <toseefdar537@gmail.com>
2024-11-11 06:16:33 -0800

    fn

--- src/server.ts
+++ src/server.ts
@@ -12,17 +12,9 @@ dbConnect();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://foodmine-frontend-gamma.vercel.app',
-    credentials: true,
+  origin: '*',
+  credentials: true,
 }));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://foodmine-frontend-gamma.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
 
 app.use("/api/foods", foodRouter)
 app.use("/api/users", userRouter)
@@ -31,7 +23,7 @@ app.use("/api/orders", orderRouter)
 
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 })
 
